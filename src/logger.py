f115d97e80f3a964a03e0d1f8404dab966a35255
md-altamash1 <your-email@example.com>
2026-05-11 21:06:49 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -10,6 +10,14 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
+
 )
+
+if __name__=="__main":
+ logging.info("logging has started")
+
+ print("Log created successfully")
